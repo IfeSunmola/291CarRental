c3df7f450c5dcc9ffae0a7ee57b0b41bb89ec0fe
Ife <sunmolaife@gmail.com>
2022-06-05 20:06:16 -0600

    add vehicle fields validated

--- 291CarRental/VehicleToolsPage.cs
+++ 291CarRental/VehicleToolsPage.cs
@@ -31,8 +31,82 @@ namespace _291CarRental
         }
 
         // add 
-        private void addVehicleButton_Click(object sender, EventArgs e)
+        private void addVehicleButton_Click2(object sender, EventArgs e)
         {
+            String year = add_yearTextbox.Text.ToString();
+            String? brand;
+            var temp = add_brandCombobox.SelectedItem;
+            if (temp == null)
+            {
+                MessageBox.Show("SELECT A BRAND");
+                return;
+            }
+            else
+            {
+                brand = temp.ToString();
+            }
+            String model = add_modelTextbox.Text;
+
+            temp = add_transmissionTypeCombobox.SelectedItem;
+            String? transmissionType;
+            if (temp == null)
+            {
+                MessageBox.Show("SELECT A TRASMISSION TYPE");
+                return;
+            }
+            else
+            {
+                transmissionType = temp.ToString();
+            }
+
+            
+            String numOfSeats = add_numOfSeatsTextbox.Text.ToString();
+            String currentMileage = add_currentMileageTextbox.TextAlign.ToString();
+            
+            temp = add_colorCombobox.SelectedItem;
+            String? color;
+            if (temp == null)
+            {
+                MessageBox.Show("SELECT A COLOR");
+                return;
+            }
+            else
+            {
+                color = temp.ToString();
+            }
+
+            
+            String plateNumber = add_plateNumberTextbox.Text;
+
+            temp = add_branchCombobox.SelectedItem;
+            String? branch;
+            if (temp == null)
+            {
+                MessageBox.Show("SELECT A BRANCH");
+                return;
+            }
+            else
+            {
+                branch = temp.ToString();
+            }
+
+            temp = add_vehicleClassCombobox.SelectedItem;
+            String? vehicleClass;
+            if (temp == null)
+            {
+                MessageBox.Show("SELECT A VEHICLE CLASS");
+                return;
+            }
+            else
+            {
+                vehicleClass = temp.ToString();
+            }
+
+
+            MessageBox.Show(year + "\n" + brand + "\n" + model + "\n" + transmissionType + "\n"
+                + numOfSeats + "\n" + currentMileage + "\n" + color + "\n" + plateNumber + "\n"
+                + branch + "\n" + vehicleClass);
+
             DialogResult confirmAdding = MessageBox.Show(
                "Confirm adding of (vehicle details?)",
                "CONFIRM ADDING VEHICLE",
@@ -47,6 +121,83 @@ namespace _291CarRental
                 MessageBox.Show("VEHICLE NOT ADDED");
             }
         }
+
+        private void addVehicleButton_Click(object sender, EventArgs e)
+        {
+            bool validated = doValidation(add_yearTextbox, add_brandCombobox, add_modelTextbox, add_transmissionTypeCombobox,
+                add_numOfSeatsTextbox, add_currentMileageTextbox, add_colorCombobox, add_plateNumberTextbox, add_branchCombobox, add_vehicleClassCombobox);
+            if (validated)
+            {
+                MessageBox.Show("GOOD");
+            }
+        }
+
+        private bool doValidation(NumericUpDown year, ComboBox brandCombobox, TextBox model, ComboBox transmissionCombobox,
+            NumericUpDown numOfSeats, NumericUpDown currentMileage, ComboBox colorCombobox, TextBox plateNumber,
+            ComboBox branchCombobox, ComboBox vehicleClassCombobox)
+        {
+            if (String.IsNullOrEmpty(year.Text.ToString()))
+            {
+                MessageBox.Show("ENTER THE YEAR");
+                return false;
+            }
+
+            if (brandCombobox.SelectedItem == null)
+            {
+                MessageBox.Show("SELECT A BRAND");
+                return false;
+            }
+
+            if (String.IsNullOrEmpty(model.Text.ToString()))
+            {
+                MessageBox.Show("ENTER THE MODEL");
+                return false;
+            }
+
+            if (transmissionCombobox.SelectedItem == null)
+            {
+                MessageBox.Show("SELECT A TRANSMISSION TYPE");
+                return false;
+            }
+
+            if (String.IsNullOrEmpty(numOfSeats.Text.ToString()))
+            {
+                MessageBox.Show("ENTER THE NUMBER OF SEATS");
+                return false;
+            }
+
+            if (String.IsNullOrEmpty(currentMileage.Text.ToString()))
+            {
+                MessageBox.Show("ENTER THE MILEAGE");
+                return false;
+            }
+
+            if (colorCombobox.SelectedItem == null)
+            {
+                MessageBox.Show("SELECT A COLOR");
+                return false;
+            }
+
+            if (String.IsNullOrEmpty(plateNumber.Text.ToString()))
+            {
+                MessageBox.Show("ENTER THE PLATE NUMBER");
+                return false;
+            }
+            
+            if (branchCombobox.SelectedItem == null)
+            {
+                MessageBox.Show("SELECT A BRANCH");
+                return false;
+            }
+
+            if (vehicleClassCombobox.SelectedItem == null)
+            {
+                MessageBox.Show("SELECT A VEHICLE CLASS");
+                return false;
+            }
+            return true;
+        }
+
         // update
         private void startUpdatingButton_Click(object sender, EventArgs e)
         {
@@ -112,15 +263,13 @@ namespace _291CarRental
 
         private void fillComboBoxes()
         {
-
-
             String query = "SELECT vehicle_class FROM Vehicle_Class;" + //get vehicle classes
                 "\nSELECT branch_name FROM Branch;" + // get branch name
                 "\nSELECT Employee.branch_id" +
                 "\nFROM Employee, Branch " +
                 "\nWHERE Employee.branch_id = Branch.branch_id" +
                 "\nAND emp_id = " + empId + ";" + // get the employee branch
-                "\nSELECT DISTINCT brand FROM Vehicle;"; // get the brand
+                "\nSELECT DISTINCT brand FROM Vehicle;"; // get the brandS
 
             using (connection = new SqlConnection(connectionString))
             using (command = new SqlCommand(query, connection))
@@ -157,6 +306,16 @@ namespace _291CarRental
             add_transmissionTypeCombobox.Items.Add("HYBRID");
             add_transmissionTypeCombobox.Items.Add("MANUAL");
             add_transmissionTypeCombobox.DropDownStyle = ComboBoxStyle.DropDownList;
+            // color
+            add_colorCombobox.Items.Add("BLACK");
+            add_colorCombobox.Items.Add("BLUE");
+            add_colorCombobox.Items.Add("GREY");
+            add_colorCombobox.Items.Add("LIGHT BLUE");
+            add_colorCombobox.Items.Add("RED");
+            add_colorCombobox.Items.Add("SILVER");
+            add_colorCombobox.Items.Add("WHITE");
+            add_colorCombobox.Items.Add("YELLOW");
+            add_colorCombobox.DropDownStyle = ComboBoxStyle.DropDownList;
         }
     }
 }
