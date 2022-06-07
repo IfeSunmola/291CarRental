99390ac43f31374befd6c9c715451b3f63104ad3
Ife <sunmolaife@gmail.com>
2022-06-06 21:36:48 -0600

    database connection class set

---
+++ 291CarRental/DatabaseConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _291CarRental
+{
+  
+    internal class DatabaseConnection
+    {
+        private const String connectionString = "Server = INCOMINGVIRUSPC\\SQLEXPRESS; Database = CarRental; Trusted_Connection = yes;";
+
+        internal SqlConnection getConnection()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+
+        internal SqlCommand getCommand(String query, SqlConnection connection)
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            return command;
+        }
+    }
+}
