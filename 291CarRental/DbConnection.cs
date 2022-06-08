ebf0a6693a0d0049f3d81aa6fbd051f0a8113431
Ife <sunmolaife@gmail.com>
2022-06-07 23:58:51 -0600

    Added DbConnection

---
+++ 291CarRental/DbConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _291CarRental
+{
+    internal class DbConnection
+    {
+        internal SqlConnection connection;
+        internal SqlCommand command;
+        internal SqlDataReader reader;
+
+        internal DbConnection()
+        {
+
+        }
+    }
+}
