6e96f8604cbd56bb0bfb3ea6fb27ac56eab1939d
Ife <sunmolaife@gmail.com>
2022-06-10 20:39:19 -0600

    checkpoint

--- 291CarRental/RunCustomReportPage.Designer.cs
+++ 291CarRental/RunCustomReportPage.Designer.cs
@@ -29,14 +29,16 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.reportCombobox = new System.Windows.Forms.ComboBox();
+            this.generateButton = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.reportsDataView = new System.Windows.Forms.DataGridView();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.reportsDataView)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -48,33 +50,25 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "SELECT REPORT TO GENERATE";
             // 
-            // comboBox1
+            // reportCombobox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(384, 11);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(314, 33);
-            this.comboBox1.TabIndex = 1;
+            this.reportCombobox.FormattingEnabled = true;
+            this.reportCombobox.Location = new System.Drawing.Point(384, 11);
+            this.reportCombobox.Name = "reportCombobox";
+            this.reportCombobox.Size = new System.Drawing.Size(276, 33);
+            this.reportCombobox.TabIndex = 1;
+            this.reportCombobox.SelectedIndexChanged += new System.EventHandler(this.reportCombobox_SelectedIndexChanged);
             // 
-            // button1
+            // generateButton
             // 
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Location = new System.Drawing.Point(289, 81);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 82);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "GENERATE";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(77, 193);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(688, 225);
-            this.dataGridView1.TabIndex = 10;
+            this.generateButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.generateButton.Location = new System.Drawing.Point(311, 189);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(194, 82);
+            this.generateButton.TabIndex = 9;
+            this.generateButton.Text = "GENERATE";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
             // 
             // button4
             // 
@@ -101,30 +95,71 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.reportsDataView);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
             this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.generateButton);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.reportCombobox);
             this.panel1.Location = new System.Drawing.Point(36, 25);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(866, 435);
+            this.panel1.Size = new System.Drawing.Size(866, 558);
             this.panel1.TabIndex = 28;
+            // 
+            // reportsDataView
+            // 
+            this.reportsDataView.AllowUserToAddRows = false;
+            this.reportsDataView.AllowUserToDeleteRows = false;
+            this.reportsDataView.AllowUserToResizeColumns = false;
+            this.reportsDataView.AllowUserToResizeRows = false;
+            this.reportsDataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.reportsDataView.Location = new System.Drawing.Point(78, 293);
+            this.reportsDataView.MultiSelect = false;
+            this.reportsDataView.Name = "reportsDataView";
+            this.reportsDataView.ReadOnly = true;
+            this.reportsDataView.RowHeadersWidth = 62;
+            this.reportsDataView.RowTemplate.Height = 33;
+            this.reportsDataView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.reportsDataView.Size = new System.Drawing.Size(721, 192);
+            this.reportsDataView.TabIndex = 30;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Location = new System.Drawing.Point(108, 113);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(624, 29);
+            this.radioButton2.TabIndex = 29;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "EMPLOYEES WITH THE LEAST RENTALS";
+            this.radioButton2.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Location = new System.Drawing.Point(108, 61);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(624, 29);
+            this.radioButton1.TabIndex = 28;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "EMPLOYEES WITH THE MOST RENTALS";
+            this.radioButton1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // RunCustomReportPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(948, 485);
+            this.ClientSize = new System.Drawing.Size(948, 595);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "RunCustomReportPage";
             this.Text = "RunCustomScript";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.reportsDataView)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -132,11 +167,13 @@
         #endregion
 
         private Label label1;
-        private ComboBox comboBox1;
-        private Button button1;
-        private DataGridView dataGridView1;
+        private ComboBox reportCombobox;
+        private Button generateButton;
         private Button button4;
         private Button button2;
         private Panel panel1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private DataGridView reportsDataView;
     }
 }
