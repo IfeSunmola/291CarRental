2b47b5ae4ea2d5f1ead84f974c91efa6c19d0497
Ife <sunmolaife@gmail.com>
2022-06-12 10:13:59 -0600

    minor changes

--- 291CarRental/DbConnection.cs
+++ 291CarRental/DbConnection.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -13,66 +13,7 @@ namespace _291CarRental
         internal SqlCommand command;
         internal SqlDataReader? reader;
         internal const string connectionString = "Server = INCOMINGVIRUSPC\\SQLEXPRESS; Database = CarRental; Trusted_Connection = yes;";
-
-        internal DbConnection()
-        {
-            connection = new SqlConnection(connectionString);
-            command = new SqlCommand();
-            try
-            {
-                connection.Open();
-                command.Connection = connection;
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show(ex.Message, "SQL CONNECTION ERROR");
-            }
-        }
-
-        internal int executeNonQuery(String insertQuery)
-        {
-            command.CommandText = insertQuery;
-            return command.ExecuteNonQuery();
-        }
-
-        internal SqlDataReader executeReader(String query)
-        {// gets something from the db
-            command.CommandText = query;
-            reader = command.ExecuteReader();
-            return reader;
-        }
-
-        internal String? executeScalar(String query)
-        {
-            command.CommandText = query;
-            var result = command.ExecuteScalar();
-            if (result == null)
-            {
-                return null;
-            }
-            return result.ToString();
-        }
-    }
-}*/
-using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _291CarRental
-{
-    public class DbConnection
-    {
-        internal SqlConnection connection;
-        internal SqlCommand command;
-        internal SqlDataReader? reader;
-        // internal const string connectionString = "Server = INCOMINGVIRUSPC\\SQLEXPRESS; Database = CarRental; Trusted_Connection = yes;";
-        internal const string connectionString = "Data Source = car-rentaldb-server.database.windows.net;" +
-  "Initial Catalog = CloudCarRental;" +
-  "User id = IfeSunmola;" +
-  "Password = D8gDGJ3grz9H4PK;";
+        //internal const string connectionString = "Data Source = car-rentaldb-server.database.windows.net;" + "Initial Catalog = CloudCarRental;" + "User id = IfeSunmola;" + "Password = D8gDGJ3grz9H4PK;";
         internal DbConnection()
         {
             connection = new SqlConnection(connectionString);
