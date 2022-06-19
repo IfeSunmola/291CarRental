d351849f148fe2d3dd48ed551db25741ec41a2cf
Ife <sunmolaife@gmail.com>
2022-06-19 16:34:33 -0600

    minor changes

--- 291CarRental/LandingPage.Designer.cs
+++ 291CarRental/LandingPage.Designer.cs
@@ -42,24 +42,28 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(54)))), ((int)(((byte)(159)))));
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(180, 147);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(168, 82);
             this.button1.TabIndex = 0;
             this.button1.Text = "CUSTOMERS";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.custButton_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(54)))), ((int)(((byte)(159)))));
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.ForeColor = System.Drawing.Color.White;
             this.button2.Location = new System.Drawing.Point(435, 147);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(168, 82);
             this.button2.TabIndex = 1;
             this.button2.Text = "EMPLOYEES";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.empButton_Click);
             // 
             // empIdLabel
@@ -73,32 +77,36 @@
             // 
             // empLoginButton
             // 
+            this.empLoginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(54)))), ((int)(((byte)(159)))));
             this.empLoginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.empLoginButton.ForeColor = System.Drawing.Color.White;
             this.empLoginButton.Location = new System.Drawing.Point(123, 95);
             this.empLoginButton.Name = "empLoginButton";
             this.empLoginButton.Size = new System.Drawing.Size(168, 47);
             this.empLoginButton.TabIndex = 4;
             this.empLoginButton.Text = "LOGIN";
-            this.empLoginButton.UseVisualStyleBackColor = true;
+            this.empLoginButton.UseVisualStyleBackColor = false;
             this.empLoginButton.Click += new System.EventHandler(this.empLoginButton_Click);
             // 
             // button3
             // 
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(54)))), ((int)(((byte)(159)))));
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.ForeColor = System.Drawing.Color.White;
             this.button3.Location = new System.Drawing.Point(686, 12);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(102, 44);
             this.button3.TabIndex = 24;
             this.button3.Text = "EXIT";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // emptyTextboxLabel
             // 
             this.emptyTextboxLabel.AutoSize = true;
             this.emptyTextboxLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.emptyTextboxLabel.ForeColor = System.Drawing.Color.Red;
+            this.emptyTextboxLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(102)))), ((int)(((byte)(121)))));
             this.emptyTextboxLabel.Location = new System.Drawing.Point(212, 35);
             this.emptyTextboxLabel.Name = "emptyTextboxLabel";
             this.emptyTextboxLabel.Size = new System.Drawing.Size(160, 25);
@@ -119,7 +127,7 @@
             // loginMessageLabel
             // 
             this.loginMessageLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.loginMessageLabel.ForeColor = System.Drawing.Color.Red;
+            this.loginMessageLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(102)))), ((int)(((byte)(121)))));
             this.loginMessageLabel.Location = new System.Drawing.Point(32, 67);
             this.loginMessageLabel.Name = "loginMessageLabel";
             this.loginMessageLabel.Size = new System.Drawing.Size(353, 25);
@@ -145,11 +153,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.empLoginPanel);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "LandingPage";
