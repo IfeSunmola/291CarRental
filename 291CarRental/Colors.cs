f4828857439bc0e4abb70f09f260a558277dfaff
Ife <sunmolaife@gmail.com>
2022-06-19 21:57:50 -0600

    added files

---
+++ 291CarRental/Colors.cs
@@ -0,0 +1,10 @@
+﻿namespace _291CarRental
+{
+    internal static class Colors
+    {
+        internal static readonly Color ERROR_COLOR = Color.FromArgb(196, 18, 12);
+        internal static readonly Color PRIMARY_COLOR = Color.FromArgb(54, 86, 97);
+        internal static readonly Color SUCCESS_COLOR = Color.FromArgb(33, 196, 12);
+        internal static readonly Color BACKGRUND_COLOR = Color.FromArgb(44, 44, 44);
+    }
+}
