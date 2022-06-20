c20b4883c531161f4852a9c02e28b8b06b7dd357
Ife <sunmolaife@gmail.com>
2022-06-19 21:58:22 -0600

    all input boxes has been validated

---
+++ 291CarRental/CreateCustomerAccount.Designer.cs
@@ -0,0 +1,414 @@
+﻿namespace _291CarRental
+{
+    partial class CreateCustomerAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.firstNameText = new System.Windows.Forms.TextBox();
+            this.lastNameText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.emailText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateOfBirthPicker = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.genderCombo = new System.Windows.Forms.ComboBox();
+            this.phoneNumberText = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.unitNumText = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.streetNumText = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.streetNameText = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.cityText = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.provinceCombo = new System.Windows.Forms.ComboBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.postalCodeText = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.licenseNoText = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.createAccountButton = new System.Windows.Forms.Button();
+            this.errorMessageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(973, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CREATING CUSTOMER ACCOUNT";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FIRST NAME";
+            // 
+            // firstNameText
+            // 
+            this.firstNameText.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.firstNameText.Location = new System.Drawing.Point(36, 117);
+            this.firstNameText.MaxLength = 20;
+            this.firstNameText.Name = "firstNameText";
+            this.firstNameText.PlaceholderText = "JOHN";
+            this.firstNameText.Size = new System.Drawing.Size(150, 31);
+            this.firstNameText.TabIndex = 2;
+            // 
+            // lastNameText
+            // 
+            this.lastNameText.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.lastNameText.Location = new System.Drawing.Point(336, 117);
+            this.lastNameText.MaxLength = 20;
+            this.lastNameText.Name = "lastNameText";
+            this.lastNameText.PlaceholderText = "BEIFONG";
+            this.lastNameText.Size = new System.Drawing.Size(150, 31);
+            this.lastNameText.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(336, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "LAST NAME";
+            // 
+            // emailText
+            // 
+            this.emailText.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.emailText.Location = new System.Drawing.Point(592, 117);
+            this.emailText.MaxLength = 50;
+            this.emailText.Name = "emailText";
+            this.emailText.PlaceholderText = "BEIFONGJOHN@GMAIL.COM";
+            this.emailText.Size = new System.Drawing.Size(302, 31);
+            this.emailText.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(592, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "EMAIL ADDRESS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "DATE OF BIRTH";
+            // 
+            // dateOfBirthPicker
+            // 
+            this.dateOfBirthPicker.Location = new System.Drawing.Point(36, 217);
+            this.dateOfBirthPicker.Name = "dateOfBirthPicker";
+            this.dateOfBirthPicker.Size = new System.Drawing.Size(233, 31);
+            this.dateOfBirthPicker.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(336, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "GENDER";
+            // 
+            // genderCombo
+            // 
+            this.genderCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.genderCombo.FormattingEnabled = true;
+            this.genderCombo.Location = new System.Drawing.Point(336, 215);
+            this.genderCombo.Name = "genderCombo";
+            this.genderCombo.Size = new System.Drawing.Size(150, 33);
+            this.genderCombo.TabIndex = 10;
+            this.genderCombo.SelectedIndexChanged += new System.EventHandler(this.genderCombo_SelectedIndexChanged);
+            // 
+            // phoneNumberText
+            // 
+            this.phoneNumberText.Location = new System.Drawing.Point(590, 217);
+            this.phoneNumberText.MaxLength = 14;
+            this.phoneNumberText.Name = "phoneNumberText";
+            this.phoneNumberText.PlaceholderText = "204-021-3012";
+            this.phoneNumberText.Size = new System.Drawing.Size(165, 31);
+            this.phoneNumberText.TabIndex = 12;
+            this.phoneNumberText.TextChanged += new System.EventHandler(this.phoneNumberText_TextChanged);
+            this.phoneNumberText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ignoreCharInput);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(590, 189);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(147, 25);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "PHONE NUMBER";
+            // 
+            // unitNumText
+            // 
+            this.unitNumText.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.unitNumText.Location = new System.Drawing.Point(36, 311);
+            this.unitNumText.MaxLength = 10;
+            this.unitNumText.Name = "unitNumText";
+            this.unitNumText.PlaceholderText = "12A";
+            this.unitNumText.Size = new System.Drawing.Size(127, 31);
+            this.unitNumText.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(36, 283);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(127, 25);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "UNIT NUMBER";
+            // 
+            // streetNumText
+            // 
+            this.streetNumText.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.streetNumText.Location = new System.Drawing.Point(336, 311);
+            this.streetNumText.MaxLength = 10;
+            this.streetNumText.Name = "streetNumText";
+            this.streetNumText.PlaceholderText = "1234";
+            this.streetNumText.Size = new System.Drawing.Size(182, 31);
+            this.streetNumText.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(336, 283);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(145, 25);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "STREET NUMBER";
+            // 
+            // streetNameText
+            // 
+            this.streetNameText.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.streetNameText.Location = new System.Drawing.Point(592, 311);
+            this.streetNameText.MaxLength = 50;
+            this.streetNameText.Name = "streetNameText";
+            this.streetNameText.PlaceholderText = "69TH STREET SW";
+            this.streetNameText.Size = new System.Drawing.Size(349, 31);
+            this.streetNameText.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(592, 283);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(124, 25);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "STREET NAME";
+            // 
+            // cityText
+            // 
+            this.cityText.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.cityText.Location = new System.Drawing.Point(36, 400);
+            this.cityText.MaxLength = 15;
+            this.cityText.Name = "cityText";
+            this.cityText.PlaceholderText = "FIRE PALACE";
+            this.cityText.Size = new System.Drawing.Size(163, 31);
+            this.cityText.TabIndex = 20;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(36, 372);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 25);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "CITY";
+            // 
+            // provinceCombo
+            // 
+            this.provinceCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.provinceCombo.FormattingEnabled = true;
+            this.provinceCombo.Location = new System.Drawing.Point(336, 398);
+            this.provinceCombo.Name = "provinceCombo";
+            this.provinceCombo.Size = new System.Drawing.Size(150, 33);
+            this.provinceCombo.TabIndex = 22;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(336, 372);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(96, 25);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "PROVINCE";
+            // 
+            // postalCodeText
+            // 
+            this.postalCodeText.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.postalCodeText.Location = new System.Drawing.Point(592, 400);
+            this.postalCodeText.MaxLength = 7;
+            this.postalCodeText.Name = "postalCodeText";
+            this.postalCodeText.PlaceholderText = "A1A-2B4";
+            this.postalCodeText.Size = new System.Drawing.Size(163, 31);
+            this.postalCodeText.TabIndex = 24;
+            this.postalCodeText.TextChanged += new System.EventHandler(this.postalCodeText_TextChanged);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(592, 372);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(126, 25);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "POSTAL CODE";
+            // 
+            // licenseNoText
+            // 
+            this.licenseNoText.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.licenseNoText.Location = new System.Drawing.Point(811, 217);
+            this.licenseNoText.MaxLength = 8;
+            this.licenseNoText.Name = "licenseNoText";
+            this.licenseNoText.PlaceholderText = "LICENSE NUMBER";
+            this.licenseNoText.Size = new System.Drawing.Size(163, 31);
+            this.licenseNoText.TabIndex = 26;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(811, 189);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(174, 25);
+            this.label14.TabIndex = 25;
+            this.label14.Text = "DRIVER LICENSE NO";
+            // 
+            // createAccountButton
+            // 
+            this.createAccountButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.createAccountButton.Location = new System.Drawing.Point(380, 489);
+            this.createAccountButton.Name = "createAccountButton";
+            this.createAccountButton.Size = new System.Drawing.Size(263, 62);
+            this.createAccountButton.TabIndex = 27;
+            this.createAccountButton.Text = "CREATE ACCOUNT";
+            this.createAccountButton.UseVisualStyleBackColor = true;
+            this.createAccountButton.Click += new System.EventHandler(this.createAccountButton_Click);
+            // 
+            // errorMessageLabel
+            // 
+            this.errorMessageLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.errorMessageLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorMessageLabel.Location = new System.Drawing.Point(36, 452);
+            this.errorMessageLabel.Name = "errorMessageLabel";
+            this.errorMessageLabel.Size = new System.Drawing.Size(949, 25);
+            this.errorMessageLabel.TabIndex = 49;
+            this.errorMessageLabel.Text = "HEHE HEHE SHEE UP SD SHEESH SS HEHE";
+            this.errorMessageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.errorMessageLabel.Visible = false;
+            // 
+            // CreateCustomerAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 563);
+            this.Controls.Add(this.errorMessageLabel);
+            this.Controls.Add(this.createAccountButton);
+            this.Controls.Add(this.licenseNoText);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.postalCodeText);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.provinceCombo);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.cityText);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.streetNameText);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.streetNumText);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.unitNumText);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.phoneNumberText);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.genderCombo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dateOfBirthPicker);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.emailText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lastNameText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.firstNameText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CreateCustomerAccount";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CreateCustomerAccount";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox firstNameText;
+        private TextBox lastNameText;
+        private Label label3;
+        private TextBox emailText;
+        private Label label4;
+        private Label label5;
+        private DateTimePicker dateOfBirthPicker;
+        private Label label6;
+        private ComboBox genderCombo;
+        private TextBox phoneNumberText;
+        private Label label7;
+        private TextBox unitNumText;
+        private Label label8;
+        private TextBox streetNumText;
+        private Label label9;
+        private TextBox streetNameText;
+        private Label label10;
+        private TextBox cityText;
+        private Label label11;
+        private ComboBox provinceCombo;
+        private Label label12;
+        private TextBox postalCodeText;
+        private Label label13;
+        private TextBox licenseNoText;
+        private Label label14;
+        private Button createAccountButton;
+        private Label errorMessageLabel;
+    }
+}
---
+++ 291CarRental/CreateCustomerAccount.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _291CarRental
+{
+    public partial class CreateCustomerAccount : Form
+    {
+        private DbConnection connection;
+        public CreateCustomerAccount(DbConnection connection)
+        {
+            InitializeComponent();
+            this.connection = connection;
+            fillComboBoxes();
+            dateOfBirthPicker.Value = DateTime.Now.AddYears(-23);
+        }
+        
+        private void fillComboBoxes()
+        {
+            genderCombo.Items.Add("SELECT ONE");
+            genderCombo.SelectedIndex = 0;
+            genderCombo.Items.Add("FEMALE");
+            genderCombo.Items.Add("MALE");
+            genderCombo.Items.Add("OTHER (TYPE)");
+
+            provinceCombo.Items.Add("SELECT ONE");
+            provinceCombo.SelectedIndex = 0;
+            provinceCombo.Items.Add("AB");
+            provinceCombo.Items.Add("BC");
+            provinceCombo.Items.Add("MB");
+            provinceCombo.Items.Add("NB");
+            provinceCombo.Items.Add("NL");
+            provinceCombo.Items.Add("NS");
+            provinceCombo.Items.Add("NT");
+            provinceCombo.Items.Add("ON");
+            provinceCombo.Items.Add("PE");
+            provinceCombo.Items.Add("QC");
+            provinceCombo.Items.Add("SK");
+        }
+       
+        //validate information in the textbox
+        private bool validateInfo()
+        { 
+            int age = (int)((DateTime.Now - dateOfBirthPicker.Value).TotalDays / 365.242199);
+            bool result = false;
+            errorMessageLabel.Visible = true;
+            //first and last name validations
+            if (String.IsNullOrEmpty(firstNameText.Text) || String.IsNullOrEmpty(lastNameText.Text))
+            {
+                errorMessageLabel.Text = "FIRST/LAST NAME ARE REQUIRED";
+            }
+            
+            //email validation
+            else if (String.IsNullOrEmpty(emailText.Text))
+            {
+                errorMessageLabel.Text = "EMAIL IS REQUIRED";
+            }
+            else if (!Regex.IsMatch(emailText.Text, "[A-Z]+@[A-Z]+\\.[A-Z]+")){
+                // email is not valid
+                errorMessageLabel.Text = "INVALID EMAIL FORMAT";
+            }
+            
+            // date of birth/age validations
+            else if (age < 23)
+            {
+                errorMessageLabel.Text = "CUSTOMER NEEDS TO BE 23 YEARS AND ABOVE";
+            }
+            
+            //gender validations
+            else if (genderCombo.SelectedIndex == 0 || String.IsNullOrEmpty(genderCombo.Text))
+            {
+                errorMessageLabel.Text = "GENDER IS REQUIRED";
+            }
+            
+            //phone number validations
+            else if (String.IsNullOrEmpty(phoneNumberText.Text))
+            {
+                errorMessageLabel.Text = "PHONE NUMBER IS REQUIRED";   
+            }
+            else if (stripPhoneNumber(phoneNumberText.Text).Length != 10)
+            {
+                errorMessageLabel.Text = "PHONE NUMBER MUST BE 10 DIGITS";
+            }
+            
+            //license number validations
+            else if (String.IsNullOrEmpty(licenseNoText.Text))
+            {
+                errorMessageLabel.Text = "LICENSE NUMBER IS REQUIRED";
+            }
+            else if (licenseNoText.Text.Length != 8)
+            {
+                errorMessageLabel.Text = "LICENSE NUMBER MUST BE 8 CHARACTERS";
+            }
+            
+            //address validations
+            else if (String.IsNullOrEmpty(unitNumText.Text) || String.IsNullOrEmpty(streetNumText.Text)
+                || String.IsNullOrEmpty(streetNameText.Text) || String.IsNullOrEmpty(cityText.Text)
+                || provinceCombo.SelectedIndex == 0 || String.IsNullOrEmpty(postalCodeText.Text))
+            {
+                errorMessageLabel.Text = "FULL ADDRESS IS REQUIRED";
+            }
+            else if (postalCodeText.Text.Length != 7)
+            {
+                errorMessageLabel.Text = "POSTAL CODE MUST BE 7 CHARACTERS";
+            }
+            else if (!Regex.IsMatch(postalCodeText.Text, "[A-Z][0-9][A-Z][-][0-9][A-Z][0-9]"))
+            {
+                errorMessageLabel.Text = "POSTAL CODE MUST BE FORM A1B-2C3";
+            }
+            
+            // all input are valid
+            else
+            {
+                result = true;
+                errorMessageLabel.Visible = false;
+            }
+            
+
+            return result;
+        }
+
+        private String stripPhoneNumber(String phoneNumber)
+        {
+            return phoneNumberText.Text.Replace("(", "").Replace(")", "").Replace("-", "").Replace(" ", "");
+        }
+        private void createAccountButton_Click(object sender, EventArgs e)
+        {
+            validateInfo();
+        }
+
+        // this method makes textboxes ignore inputs that aren't numbers
+        private void ignoreCharInput(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        // this method adds bracket and hypen to the customer's number as it is being typed in
+        private void phoneNumberText_TextChanged(object sender, EventArgs e)
+        {
+            String currentNumber = phoneNumberText.Text;
+            if (currentNumber.Length == 3 && !currentNumber.Contains("("))
+            {// current length is 3 and there are no brackets, add branckets 
+                phoneNumberText.Text = "";
+                phoneNumberText.AppendText("(" + currentNumber + ") ");
+            }
+            if (currentNumber.Length == 9 && !currentNumber.Contains("-"))
+            {// time to add -
+                phoneNumberText.AppendText("-");
+            }
+            
+        }
+
+        // same as above, just hypen
+        private void postalCodeText_TextChanged(object sender, EventArgs e)
+        {
+            String currentPostal = postalCodeText.Text;
+            if (currentPostal.Length == 3 && !currentPostal.Contains("-"))
+            {
+                postalCodeText.Text = "";
+                postalCodeText.AppendText(currentPostal + "-");
+            }
+        }
+
+        private void genderCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (genderCombo.SelectedIndex == 3)
+            {
+                genderCombo.DropDownStyle = ComboBoxStyle.DropDown;
+            }
+            else
+            {
+                genderCombo.DropDownStyle = ComboBoxStyle.DropDownList;
+            }
+        }
+    }
+}
